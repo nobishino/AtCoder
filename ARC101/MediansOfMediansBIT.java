b3d460a453ab5f670d896c3519e66a3fdf0f80a3
John Morii <john@example.com>
2018-09-09 20:13:38 +0900

    ARC101-D BIT impl AC 617ms

--- ARC101/MediansOfMediansBIT.java
+++ ARC101/MediansOfMediansBIT.java
@@ -41,6 +41,53 @@ class Main {
         条件：x以上のmedian yが半数以上存在する
         */
         int n = numbers.length;
-        return(true);
+        int cumsum = 0;
+        long m = n;
+        long intervals = m*(m+1)/2;
+        long half = intervals/2;
+        long y = 0;
+        BIT bit = new BIT(2*n+1);
+        bit.add(n+1,1);
+        for(int i = 0; i<n; i++){
+            if(numbers[i]>=x){
+                cumsum++;
+            } else {
+                cumsum--;
+            }
+            y += bit.sum(cumsum+n+1);
+            bit.add(cumsum+n+1,1);
+        }
+        return(y >= intervals - half);
+    }
+}
+
+class BIT {
+    private int[] values;
+    private int length;
+
+    public BIT(int length) {
+        this.length = length;
+        values = new int[length+1];
+    }
+
+    public void add(int location, int value){
+        for(int x = location; x <= this.length; x += x & -x){
+            values[x] += value;
+        }
+    }
+
+    public int sum(int right){
+        int ans = 0;
+        for(int x = right; x > 0; x -= x & -x){
+            ans += values[x];
+        }
+        return ans;
+    }
+
+    public int sum(int left, int right){
+        if(left > 1)
+            return(sum(right)-sum(left-1));
+        else
+            return(sum(right));
     }
 }
