ba229b08861218b22e9781bd70840c518687d616
John Morii <john@example.com>
2018-09-09 13:52:41 +0900

    added test code for merge() and invNum()

--- ARC101/MediansOfMedians.java
+++ ARC101/MediansOfMedians.java
@@ -1,7 +1,16 @@
 import java.util.*;
-
+//MergeSortと転倒数計算メソッドの返り値につかうためだけのクラス
+class MyPair{
+    public int value;
+    public int[] list;
+}
 class Main {
     public static void main(String[] args) {
+        solve();
+        //testMerge();  
+        //testInvNum();
+    }
+    public static void solve(){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a[] = new int[n];
@@ -13,7 +22,7 @@ class Main {
 
         int ans = bSearch(0, maxim, a);
         
-        System.out.println(ans);        
+        System.out.println(ans);      
     }
     
     /*
@@ -48,10 +57,10 @@ class Main {
             }
         }
         //転倒数が全区間数の半分以下
-        int half = (((n+1)*n)/2)/2;
-        int invNumber = 0;
+        int intervals = (n*(n+1))/2;
+        int half = intervals/2; // = floor(intervals/2)
         MyPair msort = invNum(cums);
-        invNumber = msort.value;
+        int invNumber = msort.value;
         if(invNumber <= half) {
             return(true);
         } else {
@@ -123,9 +132,58 @@ class Main {
         ans.value = leftP.value + rightP.value + mergeP.value;
         return(ans);
     }
+
+    //以下mergeとinvNumのテストコード
+
+    public static void testMerge(){
+        Random rdm = new Random();
+        //test merge
+        for(int it=0;it<10;it++){
+            int[] left = new int[5];
+            int[] right = new int[5];
+            for(int i = 0; i<5; i++) {
+                left[i] = rdm.nextInt(10);
+                right[i] = rdm.nextInt(10);
+            }
+            Arrays.sort(left);
+            Arrays.sort(right);
+            printArray(left);
+            printArray(right);
+            System.out.print("->");
+            printPair(merge(left,right));
+        }
+    }
+
+    public static void testInvNum(){
+        Random rdm = new Random();
+        //test merge
+        for(int it=0;it<10;it++){
+            int[] left = new int[5];
+            //int[] right = new int[10];
+            for(int i = 0; i<5; i++) {
+                left[i] = rdm.nextInt(10);
+                //right[i] = rdm.nextInt(10);
+            }
+            MyPair mp = invNum(left);
+            printArray(left);
+            printArray(mp.list);
+            printPair(mp);
+        }
+        
+    }
+    public static void printArray(int[] array){
+        System.out.print("[");
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]);
+            System.out.print(", ");
+        }
+        System.out.println("]");        
+    }
+    public static void printPair(MyPair mp){
+        System.out.print("(");
+        System.out.print(mp.value);
+        System.out.println(")");
+    }
   }
 
-class MyPair{
-    public int value;
-    public int[] list;
-}
+
