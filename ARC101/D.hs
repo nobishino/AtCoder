0e2d1c858581b93e4a25dbf6dd85bdb85fa19476
John Morii <john@example.com>
2018-09-21 21:39:15 +0900

    README

--- ARC101/D.hs
+++ ARC101/D.hs
@@ -1,39 +1,46 @@
+module Main where
 import Control.Applicative
+
 main = do 
     getLine
     a <- map read . words <$> getLine
     print $ solve a
 
-maxim = 10^9 + 1
+maxim = 10^9 + 1 :: Int
+solve :: [Int] -> Int
 solve list = bSearch 0 maxim (judge list) 
 
+judge :: [Int] -> Int -> Bool
 judge ys x = invNum <= div (div (l*(l+1)) 2) 2
     where (invNum,_) = inversionNumber cs
           cs = convert x ys
-          l = length ys
+          l = toInteger $ length ys
+
+convert :: Int -> [Int] -> [Int]
 convert x = scanl (\acc y-> if y>=x then acc+1 else acc-1) 0
 
-bSearch ok ng condition = if ng == ok + 1 
-                          then ok
-                          else if condition mid 
-                            then bSearch mid ng condition
-                            else bSearch ok mid condition
-                        where mid = div (ok+ng) 2
+bSearch :: Int -> Int -> (Int -> Bool) -> Int
+bSearch ok ng condition  | ng == ok + 1 = ok
+                         | condition mid = bSearch mid ng condition
+                         | otherwise = bSearch ok mid condition
+                        where mid = div (ok + ng) 2
 
+merge_ :: [Int] -> [Int] -> Integer -> (Integer,[Int])
 merge_ [] right _ = (0,right)
 merge_ left [] _ = (0,left)
 merge_ left@(x:xs) right@(y:ys) leftLength = if x <= y  
-                                  then (ansx,x:xList)
-                                  else (leftLength + ansy, y:yList)
-    where (ansy, yList) = merge_ left ys leftLength
-          (ansx,xList) = merge_ xs right (leftLength-1)
+                                  then seq (ansx,xList) (ansx,x:xList)
+                                  else seq (ansy, yList) (leftLength + ansy, y:yList)
+    where (ansx,xList) = merge_ xs right (leftLength-1)
+          (ansy, yList) = merge_ left ys leftLength
 
+inversionNumber :: [Int] -> (Integer,[Int])
 inversionNumber [] = (0,[])
 inversionNumber [x] = (0,[x])
-inversionNumber xs = (a+b+c,merged)
+inversionNumber xs = seq (c,merged) (a+b+c,merged)
     where left = take half xs
           right = drop half xs
           half = length xs `div` 2
           (a,ileft) = inversionNumber left
           (b,iright) = inversionNumber right
-          (c,merged) = merge_ ileft iright half
+          (c,merged) = seq (a,b,ileft,iright) merge_ ileft iright (toInteger half):q
---
+++ ARC101/Test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]){
+        int i = Integer.MAX_VALUE;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        long j = Long.MAX_VALUE;
+        System.out.println(j);
+        j++;
+        System.out.println(j);
+        long k = i*i;
+        System.out.println(k);
+    }
+}
