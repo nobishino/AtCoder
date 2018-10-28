5244faffdbead6210008aed66959afadbdf0f730
John Morii <john@example.com>
2018-10-28 14:08:00 +0900

    D-MixingExperiment AC

--- ABC054/D-MixingExperiment.py
+++ ABC054/D-MixingExperiment.py
@@ -1,9 +1,10 @@
-import copy
+# https://beta.atcoder.jp/contests/abc054/tasks/abc054_d
+
 N,Ma,Mb = map(int,input().split())
 qa=Ma
 qb=Mb
 candidates = []
-while qa <= 400 and qb <= 400:
+while qa <= N*10 and qb <= N*10:
     candidates.append((qa,qb))
     qa += Ma
     qb += Mb
@@ -15,34 +16,24 @@ for _ in range(N):
     a.append(A)
     b.append(B)
     c.append(C)
-dp = [[[] for _ in range(401)] for _ in range(N+1)]
-dp[0][0].append([])
-for i in range(N):
-    j = i+1
-    for A in range(401):
-        dp[j][A] = copy.deepcopy(dp[j-1][A])
-        if A >= a[i]:
-            for seq in dp[j-1][A-a[i]]:
-                newseq = copy.deepcopy(seq)
-                newseq.append(i)
-                dp[j][A].append(newseq)
-
-#後半
-ans = 100000
-for cand in candidates:
-    A,B = cand
-    for bseq in dp[N][A]:
-        s = 0
-        cost = 0
-        for i in bseq:
-            s += b[i]
-            cost += c[i]
-        if s == B:
-            ans = min(ans,cost)
-if ans != 100000:
-    print(ans)
-else:
+#DP
+INF = 10**8
+dp = [[[INF for _ in range(N*10+1)] for _ in range(N*10+1)] for _ in range(N+1)]
+dp [0][0][0] = 0
+for i in range(1,N+1):
+   for j in range(N*10+1) :
+       for k in range(N*10+1):
+           x = j - a[i-1]
+           y = k - b[i-1]
+           if x>=0 and y>=0:
+               dp[i][j][k] = min(dp[i-1][j][k],dp[i-1][x][y]+c[i-1])
+           else:
+               dp[i][j][k] = dp[i-1][j][k]
+#Answer
+cost = INF
+for x,y in candidates:
+    cost = min(cost,dp[N][x][y])
+if cost == INF:
     print(-1)
-            
-
-            
+else:
+    print(cost)
