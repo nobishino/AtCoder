e66f41982cd4d879830d39512a70e96925679a6e
John Morii <john@example.com>
2018-11-10 18:19:41 +0900

    simulator worked! maybe

--- HackToTheFuture2019Qualifier/Main.java
+++ HackToTheFuture2019Qualifier/Main.java
@@ -12,8 +12,6 @@ class Main{
     static Random rnd = new Random(20181110);
     static int x=13;
     static int y=13;
-    static int dx=-1;
-    static int dy=0;
     static int score;
     static int[][] countTable = new int[m-2][m-2];
     static final long timeLimit = 2500;
@@ -25,13 +23,15 @@ class Main{
         endTime = startTime + timeLimit;
         input();
         init();
-        solve();
-        //output();
-        printCount();
+        //solve();
+        output();
+        //printCount();
         System.out.println(score);
     }
     public static void init(){
-        score = simulate().score;
+        Pair p = simulate();
+        score = p.score;
+        countTable = p.ctable;
     }
     public static void solve() {
         while(System.currentTimeMillis() < endTime){
@@ -59,106 +59,100 @@ class Main{
         bulk[i][j] = nextBulk;
         Pair newPair = simulate();
         int newScore = newPair.score;
-        int[][] ct = newPair.ctable;
         if (newScore > score){
             score = newScore;
-            countTable = ct;
+            countTable = newPair.ctable;
         } else {
             bulk[i][j] = currentBulk;
         }
     }
+    //static String[] dict = {".","D","T","L","R","#"};
     public static Pair simulate(){
+        int[] dx = {1,0,-1,0};
+        int[] dy = {0,1,0,-1};
         int[][] count = new int[m-2][m-2];
         for (int i=0;i<n;i++){
             x = 13;
             y = 13;
-            dx = -1;
-            dy = 0;
-            //static String[] dict = {".","D","T","L","R","#"};
-            for(int j=0;j<l;j++){
-                switch (bulk[x][y]) {
-                    case 0:
-                        instruction(inst[i].charAt(j));
-                        break;
-                    case 1:
-                        instruction(inst[i].charAt(j));
-                        instruction(inst[i].charAt(j));
-                        break;
-                    case 2:
-                        instruction(inst[i].charAt(j));
-                        instruction(inst[i].charAt(j));
-                        instruction(inst[i].charAt(j));
-                        break;
-                    case 3:
-                        if (inst[i].charAt(j)=='R') {
-                            //System.out.println("R!");
-                            instruction('L');
+            int dir = 2;
+            for (int j=0;j<l;j++){
+                char move = inst[i].charAt(j);
+                if(move == 'S'){
+                    int tmp=1;
+                    if(bulk[x][y]==1) {
+                        tmp=2;
+                    } else if(bulk[x][y]==2){
+                        tmp=3;
+                    }
+                    for(int k=0;k<tmp;k++){
+//                        System.out.println(dir);
+                        int nx = x + dx[dir];
+                        int ny = y + dy[dir];
+                        if((nx<0|ny<0|nx>=m-2|ny>=m-2)){
+                            break;
                         } else {
-                            instruction(inst[i].charAt(j));
+                            if(bulk[nx][ny]==5) {
+                                break;
+                            }
                         }
-                        break;
-                    case 4:
-                        if (inst[i].charAt(j)=='L') {
-                            instruction('R');
-                        } else {
-                            instruction(inst[i].charAt(j));
-                        }
-                        break;
-                    default:
-                        System.out.println("ERROR");
-                        break;
-
+                        x = nx;
+                        y = ny;
+                    }
+                } else if(move == 'L'){
+                    int bn = bulk[x][y];
+                    if(bn==1){
+                        dir = rotation(dir,2);
+                    } else if(bn==2){
+                        dir = rotation(dir,3);
+                    } else if(bn==4){
+                        dir = rotation(dir,-1);
+                    } else {
+                        dir = rotation(dir,1);
+                    }
+                } else {
+                    int bn = bulk[x][y];
+                    if(bn==1){
+                        dir = rotation(dir,-2);
+                    } else if(bn==2){
+                        dir = rotation(dir,-3);
+                    } else if(bn==3){
+                        dir = rotation(dir,1);
+                    } else {
+                        dir = rotation(dir,-1);
+                    }
                 }
             }
-            count[x][y]+=1;
+        count[x][y]++;
         }
-        int score=0;
-        for(int i=0;i<m-2;i++){
-            for(int j=0;j<m-2;j++){
-                switch(count[i][j]){
-                    case 1:
-                        score += 10;
-                        break;
-                    case 2:
-                        score += 3;
-                        break;
-                    case 3:
-                        score += 1;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+        int score = evaluate(count);
         return(new Pair(score,count));
     }
-    public static void instruction(char c){
-    //static String[] dict = {".","D","T","L","R","#"};
-        int r;
-        switch (c) {
-            case 'S':
-                int nx = x+dx;
-                int ny = y+dy;
-                if((nx<0)|(nx>=m-2)|(ny<0)|(ny>=m-2)){
-                    break;
+    public static int evaluate(int[][] c){
+        int s = 0;
+        for(int i=0;i<m-2;i++){
+            for(int j=0;j<m-2;j++){
+                int num = c[i][j];
+                if(num==1){
+                    s += 10;
+                } else if(num==2){
+                    s += 3;
+                } else if(num==3){
+                    s += 1;
                 } else {
-                    if(bulk[nx][ny]==5) {
-                        break;
-                    }
-                    x = nx;
-                    y = ny;
-                    break;
+                    //
                 }
-            case 'L':
-                r = dy;
-                dy = dx;
-                dx = -r;
-            case 'R':
-                r = dy;
-                dy = -dx;
-                dx = r;
-            default:
-                break;
+            }
+        }
+        return(s);
+    }
+    public static int rotation(int v,int w){
+        int ans = v+w;
+        if (ans > 3){
+            return(ans-4);
+        } else if (ans < 0){
+            return(ans+4);
+        } else {
+            return(ans);
         }
     }
     public static void input(){
@@ -194,6 +188,7 @@ class Main{
         for(int i=0;i<m-2;i++){
             for(int j=0;j<m-2;j++){
                 System.out.print(countTable[i][j]);
+                System.out.print(',');
                 sum+=countTable[i][j];
             }
             System.out.println();
