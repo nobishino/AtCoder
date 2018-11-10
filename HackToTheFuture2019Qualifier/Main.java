c1750c383ebbe307a9e5ab8a5a21ee419ab15795
John Morii <john@example.com>
2018-11-10 17:37:21 +0900

    haki

--- HackToTheFuture2019Qualifier/Main.java
+++ HackToTheFuture2019Qualifier/Main.java
@@ -10,38 +10,71 @@ class Main{
     static int[][] bulk = new int[m-2][m-2];
     static String[] dict = {".","D","T","L","R","#"};
     static Random rnd = new Random(20181110);
-    static int x=14;
-    static int y=14;
-    static int dx=0;
-    static int dy=1;
+    static int x=13;
+    static int y=13;
+    static int dx=-1;
+    static int dy=0;
     static int score;
+    static int[][] countTable = new int[m-2][m-2];
+    static final long timeLimit = 2500;
+    static long startTime;
+    static long endTime;
+    static long repetition=1;
     public static void main(String[] args){
+        startTime = System.currentTimeMillis();
+        endTime = startTime + timeLimit;
         input();
         init();
         solve();
-        output();
+        //output();
+        printCount();
+        System.out.println(score);
     }
     public static void init(){
-        score = simulate();
+        score = simulate().score;
     }
-    public static void solve(){
+    public static void solve() {
+        while(System.currentTimeMillis() < endTime){
+            deviate();
+        }
+    }
+    public static void randomOrEmpty(){
         for (int i=0;i<m-2;i++){
             for(int j=0;j<m-2;j++){
                 bulk[i][j] = rnd.nextInt(6); //0-5 integer
             }
         }
-        int newscore = simulate();
+        int newscore = simulate().score;
         if (newscore < score){
             bulk = new int[m-2][m-2];
         }
     }
-    public static int simulate(){
+    public static void deviate(){
+        repetition++;
+        int i = rnd.nextInt(m-2);
+        int j = rnd.nextInt(m-2);
+        int currentBulk = bulk[i][j];
+        int nextBulk = rnd.nextInt(6);
+        while(nextBulk==currentBulk) nextBulk = rnd.nextInt(6);
+        bulk[i][j] = nextBulk;
+        Pair newPair = simulate();
+        int newScore = newPair.score;
+        int[][] ct = newPair.ctable;
+        if (newScore > score){
+            score = newScore;
+            countTable = ct;
+        } else {
+            bulk[i][j] = currentBulk;
+        }
+    }
+    public static Pair simulate(){
         int[][] count = new int[m-2][m-2];
         for (int i=0;i<n;i++){
-            x = 14;
-            y = 14;
-            dx = 0;
-            dy = 1;
+            x = 13;
+            y = 13;
+            dx = -1;
+            dy = 0;
+            //static String[] dict = {".","D","T","L","R","#"};
             for(int j=0;j<l;j++){
                 switch (bulk[x][y]) {
                     case 0:
@@ -58,6 +91,7 @@ class Main{
                         break;
                     case 3:
                         if (inst[i].charAt(j)=='R') {
+                            //System.out.println("R!");
                             instruction('L');
                         } else {
                             instruction(inst[i].charAt(j));
@@ -71,6 +105,7 @@ class Main{
                         }
                         break;
                     default:
+                        System.out.println("ERROR");
                         break;
 
                 }
@@ -95,15 +130,16 @@ class Main{
                 }
             }
         }
-        return(score);
+        return(new Pair(score,count));
     }
     public static void instruction(char c){
+    //static String[] dict = {".","D","T","L","R","#"};
         int r;
         switch (c) {
             case 'S':
                 int nx = x+dx;
                 int ny = y+dy;
-                if(nx<0||nx>=m-2|ny<0|ny>=m-2){
+                if((nx<0)|(nx>=m-2)|(ny<0)|(ny>=m-2)){
                     break;
                 } else {
                     if(bulk[nx][ny]==5) {
@@ -135,6 +171,7 @@ class Main{
 
     }
     public static void output(){
+    //static String[] dict = {".","D","T","L","R","#"};
         for (int j=0;j<m;j++){
             System.out.print("#");
         }
@@ -152,5 +189,24 @@ class Main{
         }
         System.out.println();
     }
+    public static void printCount(){
+        int sum=0;
+        for(int i=0;i<m-2;i++){
+            for(int j=0;j<m-2;j++){
+                System.out.print(countTable[i][j]);
+                sum+=countTable[i][j];
+            }
+            System.out.println();
+        }
+        System.out.println(sum);
+    }
 }
 
+class Pair{
+    public int score;
+    public int[][] ctable;
+    public Pair(int s, int[][] c){
+        this.score=s;
+        this.ctable = c;
+    }
+}
