8bdc897742017cd40a6023e5e9e4c4548344705a
nobishino <nobishino@nobishino.com>
2019-10-16 21:03:36 +0900

    Add ABC027-D Java

---
+++ ABC/027/D/Main.java
@@ -0,0 +1,147 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static String S;
+    static ArrayList<Integer> M = new ArrayList<Integer>();
+    public static void main(String[] args) {
+        S = sc.nextStr();
+        N = S.length();
+        int sum = 0;
+        for (int i = N-1; i>=0; i--) {
+            char c = S.charAt(i);
+            if (c == '+') {
+                sum++;
+            } else if (c == '-') {
+                sum--;
+            } else {
+                M.add(sum);
+            }
+        }
+        Collections.sort(M);
+        int answer = 0;
+        for (int i = 0; i < M.size(); i++) {
+            if (i < M.size()/2) {
+                answer -= M.get(i);
+            } else {
+                answer += M.get(i);
+            }
+        }
+        writer.println(answer); 
+        writer.flush();
+    }
+
+    static final FastScanner sc = new FastScanner(System.in);
+    static PrintWriter writer = new PrintWriter(System.out);
+
+    static class FastScanner {
+        Reader input;
+ 
+        FastScanner() {
+            this(System.in);
+        }
+ 
+        FastScanner(InputStream stream) {
+            this.input = new BufferedReader(new InputStreamReader(stream));
+        }
+ 
+        int nextInt() {
+            return (int) nextLong();
+        }
+ 
+        long nextLong() {
+            try {
+                int sign = 1;
+                int b = input.read();
+                while ((b < '0' || '9' < b) && b != '-' && b != '+') {
+                    b = input.read();
+                }
+                if (b == '-') {
+                    sign = -1;
+                    b = input.read();
+                } else if (b == '+') {
+                    b = input.read();
+                }
+                long ret = b - '0';
+                while (true) {
+                    b = input.read();
+                    if (b < '0' || '9' < b) return ret * sign;
+                    ret *= 10;
+                    ret += b - '0';
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return -1;
+            }
+        }
+ 
+        double nextDouble() {
+            try {
+                double sign = 1;
+                int b = input.read();
+                while ((b < '0' || '9' < b) && b != '-' && b != '+') {
+                    b = input.read();
+                }
+                if (b == '-') {
+                    sign = -1;
+                    b = input.read();
+                } else if (b == '+') {
+                    b = input.read();
+                }
+                double ret = b - '0';
+                while (true) {
+                    b = input.read();
+                    if (b < '0' || '9' < b) break;
+                    ret *= 10;
+                    ret += b - '0';
+                }
+                if (b != '.') return sign * ret;
+                double div = 1;
+                b = input.read();
+                while ('0' <= b && b <= '9') {
+                    ret *= 10;
+                    ret += b - '0';
+                    div *= 10;
+                    b = input.read();
+                }
+                return sign * ret / div;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return Double.NaN;
+            }
+        }
+ 
+        char nextChar() {
+            try {
+                int b = input.read();
+                while (Character.isWhitespace(b)) {
+                    b = input.read();
+                }
+                return (char) b;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return 0;
+            }
+        }
+ 
+        String nextStr() {
+            try {
+                StringBuilder sb = new StringBuilder();
+                int b = input.read();
+                while (Character.isWhitespace(b)) {
+                    b = input.read();
+                }
+                while (b != -1 && !Character.isWhitespace(b)) {
+                    sb.append((char) b);
+                    b = input.read();
+                }
+                return sb.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "";
+            }
+        }
+    }
+    //FAST SCANNER END HERE
+}
